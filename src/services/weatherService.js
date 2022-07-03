5b681192ba2bc271ec763fd7616c7195761f6dba
ritwik dalmia <dalmiaritwik@gmail.com>
2022-07-04 01:58:03 +0530

    Update weatherService.js

--- src/services/weatherService.js
+++ src/services/weatherService.js
@@ -3,7 +3,7 @@ import { DateTime } from "luxon";
 const API_KEY = "enter your api key";
 const BASE_URL = "https://api.openweathermap.org/data/2.5";
 
-// https://api.openweathermap.org/data/2.5/onecall?lat=48.8534&lon=2.3488&exclude=current,minutely,hourly,alerts&appid=1fa9ff4126d95b8db54f3897a208e91c&units=metric
+// https://api.openweathermap.org/data/2.5/onecall?lat=48.8534&lon=2.3488&exclude=current,minutely,hourly,alerts&appid=api_key&units=metric
 
 const getWeatherData = (infoType, searchParams) => {
   const url = new URL(BASE_URL + "/" + infoType);
